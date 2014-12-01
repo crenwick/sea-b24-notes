72478f8a5d6437653072e70cffad62a4aaebf578
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-30 22:07:44 -0800

    react test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,93 @@
 'use strict';
 
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-jscs');
-    grunt.loadNpmTasks('grunt-mongo-drop');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-mongo-drop');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-react');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        jscs: {
-            src: ['models/**/*.js', 'server.js', 'routes/**/*.js'],
-            options: {
-                config: '.jscsrc'
-            }
-        },
+    jscs: {
+      src: ['models/**/*.js', 'server.js', 'routes/**/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
 
-        jshint: {
-            options: {
-              jshintrc: '.jshintrc'
-            },
-            src: ['models/**/*.js', 'server.js', 'routes/**/*.js']
-        },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      src: ['models/**/*.js', 'server.js', 'routes/**/*.js']
+    },
 
-        simplemocha: {
-            src: ['test/api/**/*.js']
-        },
+    simplemocha: {
+      src: ['test/api/**/*.js']
+    },
 
-        mongo_drop: {
-            test: {
-                'uri': 'mongodb://localhost/notes_test',
-            }
+    mongo_drop: {
+      test: {
+        'uri': 'mongodb://localhost/notes_test',
+      }
+    },
+
+    clean: {
+      dev: {
+        src: ['build/']
+      },
+      react: {
+        src: ['app/react/build/']
+      }
+    },
+
+    copy: {
+      dev: {
+        cwd: 'app/',
+        src: ['**/*.html', 'css/**/*.css'],
+        expand: true,
+        dest: 'build/'
+      }
+    },
+
+    browserify: {
+      dev: {
+        src: ['app/js/**/*.js', 'app/react/build/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['debowerify']
         }
-    });
+      },
+      test: {
+        src: ['test/client/**/*.js'],
+        dest: 'test/test_bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+    },
 
-    grunt.registerTask('test', ['jshint', 'jscs', 'mongo_drop', 'simplemocha']);
-    grunt.registerTask('default', ['test']);
+    react: {
+      files: {
+        expand: true,
+        cwd: 'app/react',
+        src: ['**/*.jsx'],
+        dest: 'app/react/build',
+        ext: '.js'
+      }
+    }
+  });
+
+  grunt.registerTask('lint', ['jshint', 'jscs']);
+  grunt.registerTask('test', ['lint', 'mongo_drop', 'simplemocha']);
+  grunt.registerTask('build:react', ['clean:react', 'react']);
+  grunt.registerTask('build:dev', ['build:react', 'clean:dev', 'lint', 'browserify:dev', 'copy:dev']);
+  grunt.registerTask('build:test', ['browserify:test']);
+  grunt.registerTask('default', ['test']);
 };
