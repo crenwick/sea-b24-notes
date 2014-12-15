e804f79a5b326796be6393c54d11eceea9206761
Charles Renwick <crenwick@crenwick-mac.local>
2014-12-14 19:49:19 -0800

    building auth service

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       options: {
         node: true
       },
-      src: ['models/**/*.js', 'server.js', 'routes/**/*.js', 'app/js/**/*.js']
+      src: ['models/**/*.js', 'lib/**/*.js',  'server.js', 'routes/**/*.js', 'app/js/**/*.js']
     },
 
     simplemocha: {
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
+var passport = require('passport');
 var app = express();
 
 app.use(bodyparser.json());
 app.use(express.static(__dirname + '/build'));
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
+app.use(passport.initialize());
 
-require('./routes/notes_routes')(app);
+require('./lib/passport')(passport);
+var jwtauth = require('./lib/jwt_auth')(app.get('jwtSecret'));
+
+var notesRouter = express.Router();
+notesRouter.use(jwtauth);
+
+require('./routes/users_routes')(app, passport);
+require('./routes/notes_routes')(notesRouter);
+app.use('./', notesRouter);
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
